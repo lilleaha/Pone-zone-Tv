0f950a26bc765a971715851eab407d024e118ac2
andreas <lilleaha1@gmail.com>
2015-03-27 11:15:40 +0100

    update 0,81

---
+++ Pone Tv/App_Data/dbo.Table.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[Table]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [First and second name ] NVARCHAR(50) NOT NULL, 
+    [last name] NVARCHAR(25) NOT NULL, 
+    [Username] VARCHAR(15) NOT NULL, 
+    [Email] NVARCHAR(MAX) NOT NULL, 
+    [Phone] NCHAR(10) NULL, 
+    [Url] NVARCHAR(MAX) NULL, 
+    [Gender] NVARCHAR(1) NOT NULL, 
+    [Profile Photo] IMAGE NOT NULL, 
+    [Birthday] DATE NOT NULL
+)
